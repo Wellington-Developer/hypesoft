4637a6d6797c1135ccc50d988fc6ea70b34619c6
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-08 15:40:23 -0300

    chore: jest depedencies are configured and installed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,5 +14,5 @@ export default function Home() {
         <h1>Lets build this amazing app!</h1>
       </main>
     </>
-  )
+  );
 }
