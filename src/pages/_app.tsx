f105228def19f981e38cd5f5821c7578ce4283c9
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-08 17:38:00 -0300

    chore: base url for imports are added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 
 // Global Styles
-import GlobalStyles from "../styles/global";
+import GlobalStyles from "@/styles/global";
 
 function App({ Component, pageProps }: AppProps) {
     return (
