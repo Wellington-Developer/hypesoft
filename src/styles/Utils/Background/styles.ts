089f3bb5339e99d3012fd48ce744d23cc1662ff8
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-08 23:54:31 -0300

    feat: background purple position

--- src/styles/Utils/Background/styles.ts
+++ src/styles/Utils/Background/styles.ts
@@ -10,10 +10,9 @@ export const BgContainer = styled.div`
     grid-template-columns: 1fr 896px 425px 1fr;
     transition: all 3s ease-in-out;
     z-index: -1;
-    margin-top: 10px;
     
     .box {
-        height: calc(96px + 100vh);
+        height: calc(106px + 100vh);
         grid-row: 1;
         grid-column: 3 / 5;
         background: var(--color-bg-header);
