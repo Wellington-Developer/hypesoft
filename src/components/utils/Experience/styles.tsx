9c65578152fd572f15d7a86a0e5890aa2233ce62
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-09 15:09:47 -0300

    feat: experience component has been created

---
+++ src/components/utils/Experience/styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ExperienceData = styled.div `
+    max-width: 250px;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    h2 {
+        font-size: 5.6rem;
+        color: var(--color-pink);
+        font-weight: 400;
+    }
+    
+    h3 {
+        display: flex;
+        font-size: 1.8rem;
+        line-height: 28px;
+        color: var(--color-white);
+        font-weight: 400;
+    }
+`
