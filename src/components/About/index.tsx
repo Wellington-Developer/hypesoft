8484cddad82d65815f06fbd825749e9a881b0d37
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-09 14:27:16 -0300

    main content about has been done

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,9 +1,33 @@
+import { ExperienceData } from "../utils/Experience/styles"
 import { AboutContainer } from "./styles"
+import Image from "next/image";
+
+// Assets
+import aboutImg from '../../../public/assets/images/about.png';
+import { Button } from "../utils/Button/styles";
 
 export const About = () => {
     return (
         <AboutContainer className="container-default">
-            <h1>teste</h1>
+            <div className="left-side">
+                <Image src={ aboutImg } alt="about image" />
+            </div>
+            <div className="right-side">
+                <h1>Software Engineer and Infrastructure specialist based in Brazil.</h1>
+                <p>Morbi quam velit, euismod in imperdiet vitae, elementum et elit. Nunc finibus, felis sit amet sollicitudin sollicitudin, nisi magna feugiat enim, in maximus urna enim ac tortor. Nunc in volutpat ipsum, molestie commodo odio. Quisque auctor nisi mi. Aenean venenatis sapien et interdum interdum.</p>
+                <div className="container-experience">
+                    <ExperienceData>
+                        <h2>17+</h2>
+                        <h3>Year of experience</h3>
+                    </ExperienceData>
+
+                    <ExperienceData>
+                        <h2>325+</h2>
+                        <h3>Completed projects</h3>
+                    </ExperienceData>
+                </div>
+                <Button primary>Say hi</Button>
+            </div>
         </AboutContainer>
     )
 }
--- src/components/About/styles.tsx
+++ src/components/About/styles.tsx
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
 export const AboutContainer = styled.div`
-    border: 1px solid red;
+    display: flex;
+    align-items: center;
+    gap: 88px;
+    padding-top: 103px;
+    padding-bottom: 153px;
+
+
+
+    .left-side {
+        width: 100%;
+        max-width: 560px;
+        border-radius: 10px;
+        box-shadow: 32px 32px white;
+    }
+    
+    .right-side {
+        width: 100%;
+
+        h1 {
+            font-size: 4.8rem;
+            line-height: 117%;
+            font-weight: 500;
+            color: var(--color-white);
+            padding-bottom: 20px;
+        }
+
+        p {
+            font-size: 1.8rem;
+            line-height: 156%;
+            color: var(--color-gray-500);
+            padding-bottom: 32px;
+        }
+
+        .container-experience {
+            display: flex;
+            gap: 20px;
+            margin-bottom: 32px;
+        }
+    }
 `
