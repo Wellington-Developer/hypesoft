39f1ffc0788bc0d0c981748bc1c6c27e744c5a12
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-04-11 11:19:46 -0300

    test: about content has been tested

--- __test__/components/About/index.test.tsx
+++ __test__/components/About/index.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react";
 import { About } from "@/components/About";
 
 describe('<About />', () => {
-    const { container } = render(<About />);
-
+    
     it('should have if class left-side greater than 0', () => {
+        const { container } = render(<About />);
         const elementLeft = container.getElementsByClassName('left-side').length
 
         expect(elementLeft).toBeGreaterThan(0)
--- __test__/components/Intro/index.test.tsx
+++ __test__/components/Intro/index.test.tsx
@@ -8,4 +8,11 @@ describe('<Intro />', () => {
         let text = screen.getByText('Hire me');
         expect(text).toBeInTheDocument();
     });
+
+    it('Should have a social-container class in the section', () => {
+        const { container } = render(<Intro />);
+        const elementLeft = container.getElementsByClassName('social-container').length
+
+        expect(elementLeft).toBeGreaterThan(0)
+    });
 })
